ace891da6ca4cf0566b5eca78c33dc925662c4e5
Israel Calvete <icalvete@gmail.com>
2013-12-22 17:11:32 +0100

    Support for pear modules

---
+++ manifests/php5-pear.pp
@@ -0,0 +1,14 @@
+class php5::php5-pear () inherits php5::params {
+
+  anchor{'php5::php5-pear::begin':
+    before => Class['php5::php5-pear::install']
+  }
+
+  class{'php5::php5-pear::install':
+    require => Anchor['php5::php5-pear::begin']
+  }
+
+  anchor{'php5::php5-pear::end':
+    require => Class['php5::php5-pear::install']
+  }
+}
---
+++ manifests/php5-pear/install.pp
@@ -0,0 +1,6 @@
+class php5::php5-pear::install {
+
+  package {'php-pear':
+    ensure => present
+  }
+}
