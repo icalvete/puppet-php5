9c119eb231a4466046875fce8f42c39f1e9d3f4e
Israel Calvete <icalvete@gmail.com>
2015-01-25 16:11:30 +0100

    Adding xdebug config in DEV environment.

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,14 @@
 class php5::config {
 
   include  php5::php5-cli::config
+  
+  if $environment == 'DEV' {
+  
+    file {'xdebug_config':
+      ensure  => file,
+      path    => "${php5::params::php5_fpm_phpconf}/20-xdebug.ini",
+      content => template("${module_name}/xdebug.ini.erb"),
+      mode    => '0644',
+    }
+  }
 }
