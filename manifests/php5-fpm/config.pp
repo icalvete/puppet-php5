11fa60719e0e304e09d8b4fe44702d1c4b2192e3
Israel Calvete <icalvete@gmail.com>
2015-02-19 09:53:16 +0100

    xdebug config must be before fpm install.

--- manifests/php5-fpm/config.pp
+++ manifests/php5-fpm/config.pp
@@ -106,4 +106,14 @@ class php5::php5-fpm::config {
       changes => 'set display_errors On',
     }
   }
+  
+  if $environment == 'DEV' {
+  
+    file {'xdebug_config':
+      ensure  => file,
+      path    => "${php5::params::php5_fpm_phpconf}/20-xdebug.ini",
+      content => template("${module_name}/xdebug.ini.erb"),
+      mode    => '0644',
+    }
+  }
 }
