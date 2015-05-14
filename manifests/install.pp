44c9b479fb6f7a857d193cd6563d5c387b0b9909
Israel Calvete <icalvete@gmail.com>
2015-05-14 16:11:21 +0200

    Support 4 phalcon 2.0

--- manifests/install.pp
+++ manifests/install.pp
@@ -37,10 +37,16 @@ class php5::install inherits php5::params {
 
   if $php5::phalcon {
 
+    if $php5::phalcon == 2{
+      $phalcon_module = 'phalcon2.so'
+    } else {
+      $phalcon_module = 'phalcon.so'
+    }
+
     file {'phalcon.so':
       ensure => present,
       path   => "${php5::params::extension_dir}/phalcon.so",
-      source => "puppet:///modules/${module_name}/phalcon/${::osfamily}/${::lsbdistcodename}/phalcon.so",
+      source => "puppet:///modules/${module_name}/phalcon/${::osfamily}/${::lsbdistcodename}/${$phalcon_module}",
       owner  => 'root',
       group  => 'root',
       mode   => '0644',
