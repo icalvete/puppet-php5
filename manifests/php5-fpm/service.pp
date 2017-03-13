cffb9b0851495be11a2080bc612385a290e4024a
Israel Calvete <icalvete@gmail.com>
2017-03-13 09:04:55 +0100

    Upstart and Systemd support.

--- manifests/php5-fpm/service.pp
+++ manifests/php5-fpm/service.pp
@@ -1,9 +1,18 @@
 class php5::php5-fpm::service {
 
+  if versioncmp($operatingsystemrelease, '15.04') < 0 {
+
+    $provider = 'upstart'
+  } else{
+  
+    $provider = 'systemd'
+  }
+
   service { $php5::params::php5_fpm_service:
     ensure     => running,
     hasstatus  => true,
     hasrestart => true,
     enable     => true,
+    provider   => $provider
   }
 }
